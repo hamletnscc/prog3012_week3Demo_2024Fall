fd01f3c782814bed1bce414e3083dd6954688d1e
Hamlet Lin <hamlet.lin@nscc.ca>
2024-11-13 10:00:37 -0400

    add express app in myapp folder

--- httpDemo.js
+++ httpDemo.js
@@ -4,4 +4,12 @@ Date: 11/13/2024
 Description: Node + HTTP module, create a simple web server using the http module in node.js
 */
 
+//declare a http server object using the http module
 const http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+    let name = req.url;    
+    res.write(`<h1>Hello ${name}!</h1>`); //write a response to the client
+    res.end(); //end the response
+  }).listen(8080); //the server object listens on port 8080
