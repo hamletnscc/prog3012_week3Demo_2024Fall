fcfc423976c1363a3c2aa6f6ca7d80bebd17c75f
Hamlet Lin <hamlet.lin@nscc.ca>
2024-11-13 10:18:34 -0400

    complete the basic get call back function using express framework

---
+++ node-rest-api/app.js
@@ -0,0 +1,31 @@
+/*
+Name: Hamlet Lin
+Date: 11/13/2024
+Description: Create simple rest api using node and express
+*/
+
+//create a reference to the express module
+const express = require('express');
+//create an express app object using the express constructor
+const app = express();
+//indicate what format we will be using for this web service
+app.use(express.json());
+
+//declare the port to be listened to and initialize the web service
+const PORT = process.env.PORT||3000;
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+  });
+
+//declare the callback function based on the 
+//type of request (e.g. GET, POST, PUT, DELETE)
+//We are using GET in this one, thus app.get
+app.get("/status", (request, response) => {
+    //declare the response to be sent
+    //in jsons format
+    const status = {
+       "Status": "Running"
+    };    
+    //sending this back to the client
+    response.send(status);
+ });
